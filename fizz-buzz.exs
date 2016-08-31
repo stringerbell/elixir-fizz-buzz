c3afec96b86d645b980ada68352fc3a0aa49d9cb
Dann Stockton <stringerbell@users.noreply.github.com>
2016-08-31 07:00:49 -0600

    Create fizz-buzz.exs

---
+++ fizz-buzz.exs
@@ -0,0 +1,17 @@
+defmodule FizzBuzz do
+  def first(num) when is_integer(num) do
+    {num, ""}
+  end
+  def match({num, string}, div, add) when rem(num, div) == 0 do
+    {num, string <> add}
+  end
+  def match({num, string}, _div, _add) do
+    {num, string}
+  end
+  def last({num, string}) when string == "" do
+    num
+  end
+  def last({_num, string}) do
+    string
+  end
+end
